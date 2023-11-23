568428d042ae7e54926080a16e102aa1c541816d
InfinitlyUndefined <absrdreality@gmail.com>
2023-11-22 19:14:58 -0500

    Added TSUP and @swc/core

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  bundle: true,
   clean: true,
-  dts: false,
-  entry: ["src/index.ts"],
-  target: "esnext",
-  tsconfig: "tsconfig.json",
-  format: "esm",
-  minify: true,
-  splitting: true,
+  shims: false,
   outDir: "dist",
+  entry: ["src/index.ts", "src/**/*.ts"],
+  dts: false,
+  cjsInterop: true,
+  format: ["esm"],
+  keepNames: true,
   sourcemap: true,
-  shims: true,
-  platform: "node",
+  target: "esnext",
+  minify: true,
   skipNodeModulesBundle: true,
+  splitting: true,
+  tsconfig: "tsconfig.json",
 });
